69094374e825fc3f7636da3c6716b0afe10bc9f0
sjas <teilzeittresor@web.de>
2013-07-21 19:17:05 +0200

    update

--- Test.hs
+++ Test.hs
@@ -1,6 +1,12 @@
+--
+-- Contains just the single test method, to separate the code logically.
+--
+
 module Test where
 
-{-chk :: a -> a -> IO()-}
+-- TEST METHOD
+-- lacks type definition because I don't know how to 'type' it yet.
+-- thus letting haskell inferring it.
 chk a = if a == True 
-    then print "OK"
-    else print "----- TEST FAIL -----"
+        then print "OK"
+        else print "----- TEST FAIL -----"
